5ab01784e1bb21e67f85b6f0618873b53352a4e0
ishtiyaqahmed115 <33000043+ishtiyaqahmed115@users.noreply.github.com>
2020-12-09 10:05:45 -0800

    Clean Structure for ParserTree

--- src/CSC549_Fall2020_Interpreter-main_9thDec_2020/CSC549_Fall2020_Interpreter-main/src/ParserTree/ExpressionPart.java
+++
@@ -1,9 +0,0 @@
-package ParserTree;
-
-public abstract class ExpressionPart extends PartTree 
-{
-	public ExpressionPart()
-	{
-		super("ExpressionPart");
-	}
-}
--- src/CSC549_Fall2020_Interpreter-main_9thDec_2020/CSC549_Fall2020_Interpreter-main/src/ParserTree/StatementPart.java
+++
@@ -1,9 +0,0 @@
-package ParserTree;
-
-public abstract class StatementPart extends PartTree
-{
-	public StatementPart()
-	{
-		super("StatementPart");
-	}
-}
